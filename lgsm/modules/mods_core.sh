ecde04cdcf402c39afff77a2d4d47e947a844fa6
rconjoe <root@trog.host>
2024-05-12 05:39:18 -0700

    feat(mods): when removing metamodsource2, remove the line in gameinfo.gi we added at install

--- lgsm/modules/mods_core.sh
+++ lgsm/modules/mods_core.sh
@@ -767,8 +767,23 @@ fn_mod_install_gameinfo_gi_file() {
 	fi
 }
 
-# fn_mod_remove_gameinfo_gi_file() {
-# }
+fn_mod_remove_gameinfo_gi_file() {
+	if [ -f "${modinstalldir}/gameinfo.gi" ]; then
+		# modify the liblist.gam file to initialize Metamod
+		logentry="sed '/Game csgo\/addons\/metamod/d' ${modinstalldir}/gameinfo.gi > temp && mv temp ${modinstalldir}/gameinfo.gi"
+		echo -en "modifying gameinfo.gi..."
+		sed '/Game csgo\/addons\/metamod/d' ${modinstalldir}/gameinfo.gi > temp && mv temp ${modinstalldir}/gameinfo.gi
+		exitcode=$?
+		# if replacement back didn't happen, error out.
+		if [ "${exitcode}" != 0 ]; then
+			fn_script_log_fail "${logentry}"
+			fn_print_fail_eol_nl
+		else
+			fn_script_log_pass "${logentry}"
+			fn_print_ok_eol_nl
+		fi
+	fi
+}
 
 ## Database initialisation.
 
